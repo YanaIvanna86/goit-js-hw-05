00679333cf4f32429f9a56966acfc1d07b3bdb73
YanaIvanna86 <163177742+YanaIvanna86@users.noreply.github.com>
2024-06-16 19:09:07 +0300

    task-1

---
+++ js/task-2.js
@@ -0,0 +1,11 @@
+/*function calcAverageCalories(days){
+   let averageCalories = 0;
+   for (const calorie of days) {
+      averageCalories += calorie.calories/days.length;
+   }
+   return averageCalories;
+}*/
+
+
+
+ 
---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+/*const profile = {
+   username: "Jacob",
+   playTime: 300,
+   changeUsername(newName) {
+      this.username = newName;
+      },
+   updatePlayTime(hours) {
+      this.playTime += hours;
+      }, 
+    getInfo() {
+      return `"${profile.username} has ${profile.playTime} active hours!"`;
+      }
+};*/
+
